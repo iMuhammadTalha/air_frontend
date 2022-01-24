d5a11b583b661105e0912769d6ac5bfcd920f296
Ihsan Ul Haq <ihsanulhaqhaq@gmail.com>
2022-01-24 13:01:07 +0500

    Fixed port in start script.

--- src/app/server.js
+++ src/app/server.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // export const Base_URL = "http://localhost:3000/";
-export const Base_URL = "http://10.3.20.172:3000/";      //Development & testing
+export const Base_URL = "http://111.68.101.11:3000/";      //Development & testing
 
 const token = localStorage.getItem('jwtToken');
 axios.defaults.headers.common['Authorization'] = token;
